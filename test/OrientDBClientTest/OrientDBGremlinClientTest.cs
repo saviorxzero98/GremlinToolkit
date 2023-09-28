dbb99180750202f7827b8db5a763a409a6b0896f
saviorxzero98 <saviorxzero98@gmail.com>
2023-09-28 20:39:22 +0800

    add: OrientDB Client Test Project

---
+++ test/OrientDBClientTest/OrientDBGremlinClientTest.cs
@@ -0,0 +1,66 @@
+﻿using GQL.OrientDBClients.Configs;
+using GQL.QueryBuilders.Builders.Traversals;
+using Gremlin.Net.Process.Traversal;
+using Xunit;
+
+namespace GQL.OrientDBClients.Test
+{
+    public class OrientDBGremlinClientTest : IDisposable
+    {
+        private OrientDBGremlinClient _service;
+
+        public OrientDBGremlinClientTest()
+        {
+            var config = new OrientGremlinConfig().SetHost("localhost", 8182)
+                                                  .SetAccount("root", "<Your Password>");
+
+            _service = new OrientDBGremlinClient(config);
+
+            GraphTraversalSource source = _service.GetTraversalSource();
+            source.AddV("person").Property("name", "john").Iterate();
+        }
+
+        public void Dispose()
+        {
+            // Clear Edge & Vertex
+            _service.ClearAllEdge();
+            _service.ClearAllVertex();
+
+            // Close Gremlin Client
+            _service.CloseConnection();
+        }
+
+        [Fact]
+        public void TestGraphTraversal()
+        {
+            var startTime = DateTime.Now;
+
+            GraphTraversalSource source = _service.GetTraversalSource();
+
+            var result = source.V()
+                               .Has("name", TextP.Containing("john"))
+                               .Next();
+
+            var endTime = DateTime.Now;
+
+            double elapsedMs = (endTime - startTime).TotalMilliseconds;
+        }
+
+        [Fact]
+        public async Task TestSubmitAsync()
+        {
+            var startTime = DateTime.Now;
+
+            //string gremlinCommand = "g.V().has('name', containing('john'))";
+            string gremlinCommand = GraphTraversal.Graph()
+                                              .Vertex()
+                                              .Has("name", "john")
+                                              .ToString();
+
+            var result = await _service.SubmitAsync<dynamic>(gremlinCommand);
+            var endTime = DateTime.Now;
+
+            double elapsedMs = (endTime - startTime).TotalMilliseconds;
+        }
+    }
+}
---
+++ test/OrientDBClientTest/OrientDBRestClientTest.cs
@@ -0,0 +1,67 @@
+﻿using GQL.OrientDBClients.Configs;
+using GQL.OrientDBClients.Models;
+using GQL.OrientDBClients.Models.Requests;
+using GQL.QueryBuilders.Builders.Traversals;
+using Xunit;
+
+namespace GQL.OrientDBClients.Test
+{
+    public class OrientDBRestClientTest
+    {
+        private OrientDBRestClient _client;
+
+        public OrientDBRestClientTest()
+        {
+            var config = new OrientDBConfig().SetHost("localhost", 2480)
+                                             .SetAccount("root", "<Your Password>")
+                                             .SetDatabase("graphdb");
+
+            _client = new OrientDBRestClient(config);
+        }
+
+        [Fact]
+        public async Task TestPostGremlinCommand()
+        {
+            //string gremlinCommand = "g.V().hasLabel('department').toList()";
+            string gremlinCommand = GraphTraversal.Graph()
+                                              .Vertex()
+                                              .HasLabel("department")
+                                              .ToList()
+                                              .ToString();
+            var request = PostCommandRequest.Create(gremlinCommand);
+
+            var startTime = DateTime.Now;
+            var result = await _client.PostGremlinCommandAsync(request);
+            var endTime = DateTime.Now;
+
+            double elapsedMs = (endTime - startTime).TotalMilliseconds;
+        }
+
+        [Fact]
+        public async Task TestPostBatchGremlinCommand()
+        {
+            List<string> commands = new List<string>()
+            {
+                GraphTraversal.Graph().Vertex().Drop().ToString(),
+                GraphTraversal.Graph().AddVertex("test").Property("id", "0001").ToString(),
+                GraphTraversal.Graph().Vertex().ToString()
+            };
+            
+            var request = PostBatchRequest.CreateBatchGremlin(commands, transaction: true);
+            var result = await _client.PostBatchAsync(request);
+        }
+
+        [Fact]
+        public async Task TestPostSqlCommand()
+        {
+            string sqlCommand = "SELECT @rid FROM V WHERE SEARCH_INDEX(\"V.name\", \" *john*\") = true";
+            var request = PostCommandRequest.Create(sqlCommand);
+
+            var startTime = DateTime.Now;
+            var result = await _client.PostSqlCommandAsync(request);
+            var endTime = DateTime.Now;
+
+            double elapsedMs = (endTime - startTime).TotalMilliseconds;
+        }
+    }
+}
