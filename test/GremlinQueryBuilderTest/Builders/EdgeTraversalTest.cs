bc362a62ceb7abb74cbac56fb0959c314464f360
saviorxzero98 <saviorxzero98@gmail.com>
2023-09-28 17:51:56 +0800

    add: Gremlin Query Builder Test Project

---
+++ test/GremlinQueryBuilderTest/Builders/EdgeTraversalTest.cs
@@ -0,0 +1,75 @@
+﻿using GQL.QueryBuilders.Builders.Traversals;
+using Xunit;
+
+namespace GQL.QueryBuilders.Test.Builders
+{
+    public class EdgeTraversalTest
+    {
+        [Fact]
+        public void TestAddEdge()
+        {
+            // Arrange
+            string expectedA = "g.addE('create').property('weight',0.99).from(V().has('person','name','jobs steve')).to(V().has('product','name','iphone'))";
+            string expectedB = "g.V().has('person','name','jobs steve').addE('create').property('weight',0.99).to(V().has('product','name','iphone'))";
+
+            // Act
+            string actualA = GraphTraversal.Graph()
+                                       .AddEdge("create").Property("weight", 0.99)
+                                       .FromVertex((v) => v.Has("person", "name", "jobs steve"))
+                                       .ToVertex((v) => v.Has("product", "name", "iphone")).ToString();
+            string actualB = GraphTraversal.Graph()
+                                       .Vertex().Has("person", "name", "jobs steve")
+                                       .AddEdge("create").Property("weight", 0.99)
+                                       .ToVertex((v) => v.Has("product", "name", "iphone")).ToString();
+
+            // Assert
+            Assert.Equal(expectedA, actualA);
+            Assert.Equal(expectedB, actualB);
+        }
+
+        [Fact]
+        public void TestAddEdgeWithOtherGraph()
+        {
+            // Arrange
+            string expectedA = "tg.addE('create').property('weight',0.99).from(V().has('person','name','jobs steve')).to(V().has('product','name','iphone'))";
+            string expectedB = "tg.V().has('person','name','jobs steve').addE('create').property('weight',0.99).to(V().has('product','name','iphone'))";
+
+            // Act
+            string actualA = GraphTraversal.Graph("tg")
+                                       .AddEdge("create").Property("weight", 0.99)
+                                       .FromVertex((v) => v.Has("person", "name", "jobs steve"))
+                                       .ToVertex((v) => v.Has("product", "name", "iphone")).ToString();
+            string actualB = GraphTraversal.Graph("tg")
+                                       .Vertex().Has("person", "name", "jobs steve")
+                                       .AddEdge("create").Property("weight", 0.99)
+                                       .ToVertex((v) => v.Has("product", "name", "iphone")).ToString();
+
+            // Assert
+            Assert.Equal(expectedA, actualA);
+            Assert.Equal(expectedB, actualB);
+        }
+
+
+        [Fact]
+        public void TestAddEdgeWithObject()
+        {
+            // Arrange
+            string expectedA = "g.addE('create').property('weight',0.99).from(V().has('person','name','jobs steve')).to(V().has('product','name','iphone'))";
+            string expectedB = "g.V().has('person','name','jobs steve').addE('create').property('weight',0.99).to(V().has('product','name','iphone'))";
+
+            // Act
+            string actualA = GraphTraversal.Graph()
+                                       .AddEdge("create", new { weight = 0.99 })
+                                       .FromVertex("person", "name", "jobs steve")
+                                       .ToVertex("product", "name", "iphone").ToString();
+            string actualB = GraphTraversal.Graph()
+                                       .Vertex().Has("person", "name", "jobs steve")
+                                       .AddEdge("create", new { weight = 0.99 })
+                                       .ToVertex("product", "name", "iphone").ToString();
+
+            // Assert
+            Assert.Equal(expectedA, actualA);
+            Assert.Equal(expectedB, actualB);
+        }
+    }
+}
---
+++ test/GremlinQueryBuilderTest/Builders/VertexTraversalTest.cs
@@ -0,0 +1,100 @@
+﻿using GQL.QueryBuilders.Builders.Traversals;
+using Xunit;
+
+namespace GQL.QueryBuilders.Test.Builders
+{
+    public class VertexTraversalTest
+    {
+        [Fact]
+        public void TestAddVertex()
+        {
+            // Arrange
+            string expected = "g.addV('person').property('name','stephen')";
+
+            // Act
+            string actual = GraphTraversal.Graph()
+                                      .AddVertex("person")
+                                      .Property("name", "stephen")
+                                      .ToString();
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAddVertexWithOtherGraph()
+        {
+            // Arrange
+            string expected = "tg.addV('person').property('name','stephen')";
+
+            // Act
+            string actual = GraphTraversal.Graph("tg")
+                                      .AddVertex("person")
+                                      .Property("name", "stephen")
+                                      .ToString();
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAddVertexWithObject()
+        {
+            // Arrange
+            string expected = "g.addV('person').property('id',1).property('name','stephen')";
+
+            // Act
+            string actual = GraphTraversal.Graph()
+                                      .AddVertex("person", new { id = 1, name = "stephen" })
+                                      .ToString();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestGetVertexValues()
+        {
+            // Arrange
+            string expectedA = "g.V().has('person','name','stephen').values()";
+            string expectedB = "g.V().has('person','name','stephen').values('ages','cname')";
+            string expectedC = "g.V().has('person','name','stephen').valueMap()";
+            string expectedD = "g.V().has('person','name','stephen').valueMap('ages','cname')";
+            string expectedE = "g.V().has('person','name','stephen').elementMap()";
+            string expectedF = "g.V().has('person','name','stephen').elementMap('ages','cname')";
+            string expectedG = "g.V().not(hasLabel('person')).elementMap()";
+
+            // Act
+            string actualA = GraphTraversal.Graph().Vertex().Has("person", "name", "stephen").Values().ToString();
+            string actualB = GraphTraversal.Graph().Vertex().Has("person", "name", "stephen").Values("ages", "cname").ToString();
+            string actualC = GraphTraversal.Graph().Vertex().Has("person", "name", "stephen").ValueMap().ToString();
+            string actualD = GraphTraversal.Graph().Vertex().Has("person", "name", "stephen").ValueMap("ages", "cname").ToString();
+            string actualE = GraphTraversal.Graph().Vertex().Has("person", "name", "stephen").ElementMap().ToString();
+            string actualF = GraphTraversal.Graph().Vertex().Has("person", "name", "stephen").ElementMap("ages", "cname").ToString();
+            string actualG = GraphTraversal.Graph().Vertex().Not((v) => v.HasLabel("person")).ElementMap().ToString();
+
+            // Assert
+            Assert.Equal(expectedA, actualA);
+            Assert.Equal(expectedB, actualB);
+            Assert.Equal(expectedC, actualC);
+            Assert.Equal(expectedD, actualD);
+            Assert.Equal(expectedE, actualE);
+            Assert.Equal(expectedF, actualF);
+            Assert.Equal(expectedG, actualG);
+        }
+
+        [Fact]
+        public void TextGqlInjection()
+        {
+            // Arrange
+            string expected = "g.addV('person').property('name','jobs\\'stephen')";
+
+            // Act
+            string actual = GraphTraversal.Graph()
+                                      .AddVertex("person")
+                                      .Property("name", "jobs'stephen")
+                                      .ToString();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
